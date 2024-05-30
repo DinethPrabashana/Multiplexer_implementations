638eabaf107265da047e1842b45c553e1f2a3262
Dineth Perera <143341376+DinethPrabashana@users.noreply.github.com>
2024-05-30 19:34:13 +0530

    Delete 4to1_MUX_DataflowLevel.v

--- 4to1_MUX_DataflowLevel.v
+++
@@ -1,8 +0,0 @@
-module MUX4to1(out, i0, i1, i2, i3, s0, s1);
-// ports declaration wether the ports are input or output 
-input i0, i1, i2, i3 ;
-input s0, s1 ;
-output out ;
-// conditional statment
-assign out = s1?(s0?i3:i2):(s0?i1:i0);
-endmodule 
